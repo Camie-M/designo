fe73614897e6d9901622f7dbb5d3d95e6a9127d0
Camila Moura <camilamouradev@gmail.com>
2024-03-13 13:23:07 -0300

    chore: created pages and altered metadata content

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 import * as S from './styles'
 
 const Main = () => (
@@ -8,8 +6,6 @@ const Main = () => (
     <h2>Subtitle</h2>
     <h3>Title</h3>
     <p>Regular text</p>
-
-    <Image src={`/assets/circle.png`} alt={'Circle'} width={640} height={640} />
   </S.Wrapper>
 )
 
